682b51071e70116dad95219c93ee23fc5a4d2983
DoubleB00 <bbrian0325@gmail.com>
2026-02-03 18:49:48 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -119,17 +119,26 @@ function App() {
       </section>
 
       {/* Banner Separator */}
-      <section className="w-full bg-gradient-to-b from-sky-50 to-white">
-        <div className="relative w-full h-[140px] md:h-[240px] flex items-center justify-center">
-          <img
-            src="/att.ey117fo0kzzpc69gbvw5icflkk9zeieaxr0tuvedfuo.jpg"
-            srcSet="/att.ey117fo0kzzpc69gbvw5icflkk9zeieaxr0tuvedfuo.jpg 1x, /att.ey117fo0kzzpc69gbvw5icflkk9zeieaxr0tuvedfuo.jpg 2x"
-            alt="Airplane soaring through beautiful sky - ZEmKay Travels"
-            className="w-full h-full object-contain"
-            loading="lazy"
-            style={{ imageRendering: 'crisp-edges' }}
-          />
-        </div>
+      <section
+        className="relative overflow-hidden"
+        style={{
+          width: '100vw',
+          position: 'relative',
+          left: '50%',
+          right: '50%',
+          marginLeft: '-50vw',
+          marginRight: '-50vw',
+          height: 'clamp(180px, 22vw, 280px)'
+        }}
+      >
+        <img
+          src="/att.ey117fo0kzzpc69gbvw5icflkk9zeieaxr0tuvedfuo.jpg"
+          srcSet="/att.ey117fo0kzzpc69gbvw5icflkk9zeieaxr0tuvedfuo.jpg 1x, /att.ey117fo0kzzpc69gbvw5icflkk9zeieaxr0tuvedfuo.jpg 2x"
+          alt="Airplane soaring through beautiful sky - ZEmKay Travels"
+          className="w-full h-full object-cover object-center"
+          loading="lazy"
+          style={{ imageRendering: 'crisp-edges' }}
+        />
       </section>
 
       <section id="about" className="py-24 bg-white relative overflow-hidden">
